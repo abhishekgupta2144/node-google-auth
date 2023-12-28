d524b9efcb39517ced6db4651fdea8c74d860355
abhishekgupta2144 <abhishek.gupta@habilelabs.io>
2023-12-28 14:23:36 +0530

    fix: changes sign out

--- routes/auth.js
+++ routes/auth.js
@@ -94,4 +94,11 @@ router.get(
   })
 );
 
+router.post('/logout', function(req, res, next) {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
+
 module.exports = router;
